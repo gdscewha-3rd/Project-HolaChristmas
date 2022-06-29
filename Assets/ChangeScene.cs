24c7a5d78536d163aad5d00a773e3bdb3ec59f64
seoyeon ahn <61787171+sonoasy@users.noreply.github.com>
2022-06-29 20:43:48 +0900

    Add files via upload

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+   
+    public void ChangeSceneBtn()
+    {
+
+        if (this.gameObject.name == "StartButton") SceneManager.LoadScene("MainScene");
+
+    }
+
+}
---
+++ Assets/rock_move.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rock_move : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject rock;
+    private float Dist;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Dist = Vector3.Distance(Player.transform.position, gameObject.transform.position);
+    }
+    void moving()
+    {
+        
+    }
+}
